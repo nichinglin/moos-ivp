66b49dcd066b4874eda6db31be01f31285e4f14e
nichinglin <mo83117401500094@gmail.com>
2018-04-07 14:56:18 -0400

    update moos-ivp-monica/clean.sh it will clean all log file and build

--- clean.sh
+++ clean.sh
@@ -9,5 +9,6 @@ find . -name '*~'  -print -exec rm -rfv {} \;
 find . -name '#*'  -print -exec rm -rfv {} \;
 find . -name '*.moos++'  -print -exec rm -rfv {} \;
 
-#find . -name 'MOOSLog*'  -print -exec rm -rfv {} \;
+find . -name 'MOOSLog*'  -print -exec rm -rfv {} \;
+find . -name 'LOG_*'  -print -exec rm -rfv {} \;
 
